a45eb558d7d20338da8d7798ae0f342620f041da
nguyen190887 <nguyen190887@gmail.com>
2018-10-03 06:17:47 +0700

    Refactor to support routing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import App from './app';
+import Home from './components/pages/Home';
+import Router from './components/router/Router';
+import Route from './components/router/Route';
+import { history } from './history';
 
 import './shared/crash';
 import './shared/service-worker';
@@ -9,4 +13,26 @@ import './shared/vendor';
 // NOTE: this isn't ES*-compliant/possible, but works because we use Webpack as a build tool
 import './styles/styles.scss';
 
-render(<App />, document.getElementById('app'));
+// render(<App />, document.getElementById('app'));
+export const renderApp = (state, callback = () => {}) => {
+    render(
+        <Router {...state}>
+            <Route path="" component={App}>
+                <Route path="/" component={Home} />
+            </Route>
+        </Router>,
+        document.getElementById('app'),
+        callback
+    );
+};
+
+let state = {
+    location: window.location.pathname
+};
+
+history.listen(location => {
+    state = {...state, ...{ location: location.pathname }};
+    renderApp(state);
+})
+
+renderApp(state);
