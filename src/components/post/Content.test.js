4db7f4ff809ac5bdb354cb6090d1720f845e47a4
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2018-10-14 06:14:56 +0700

    Init 1st test

---
+++ src/components/post/Content.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shalow} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import {Content} from './Content';
+import { wrap } from 'rest';
+
+describe('<Content/>', () => {
+    describe('render methods', () => {
+        it('should render correctly', () => {
+            const mockPost = {
+                content: 'ReactJS is cool'
+            };
+            const wrapper = shalow(<Content post={mockPost} />);
+            expect(wrapper.find('p').length).toBe(1);
+        })
+    });
+});
